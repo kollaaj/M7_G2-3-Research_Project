e6bca4166da3f1db3876956a1a666d05e6818679
Kollaaj <kolbrunaj@gmail.com>
2021-05-03 15:50:12 +0000

    font test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
+import './fonts/halfomania-regular-webfont.woff'
 import App from './App';
 
 ReactDOM.render(
